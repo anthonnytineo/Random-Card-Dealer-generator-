eb0ec765ada2acec2e8f6199df3e8d744fd1e9a6
Anthonny Tineo Ruiz <anthonnytineo@MacBook-Pro-de-Anthonny.local>
2021-05-22 17:46:06 -0400

    estilos y JS

--- src/app.js
+++ src/app.js
@@ -1,6 +1,32 @@
-import "bootstrap";
 import "./style.css";
 
-window.onload = function() {
-  console.log("Everything is working!");
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRandomNumber());
+  document.querySelector(".card").innerHTML = generateRandomSuit();
+};
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let inexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+
+let generateRandomSuit = () => {
+  let suit = ["diamond", "spades", "heart", "club"];
+  let inexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
 };
